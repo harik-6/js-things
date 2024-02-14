54c88ecf72e9c4ba9aad39d5c442d43da83a0219
harik-6 <haribalaji32@gmail.com>
2024-02-14 18:33:03 +0530

    chore: added more code

---
+++ under-the-hood-webpack/transpiled/message.js
@@ -0,0 +1,8 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+const message = "Hello World.";
+var _default = exports.default = message;
